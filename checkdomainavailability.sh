86854f430715e664049a248e41d90a7d80d50f7a
e <e@coip.me>
2020-11-30 23:08:06 -0600

    dirstuff (#1)
    * consume list to check from /tmp/files
    
    * pathing off /tmp/files
    
    * path update
    
    * path update
    
    * update pathing

--- checkdomainavailability.sh
+++ checkdomainavailability.sh
@@ -1,15 +1,15 @@
 #!/bin/ash
 sites=$( cat $1 )
-echo -n "" > listofnames
+echo -n "" > /tmp/files/tocheck
 
 for domainname in $sites; do
     echo "checking $domainname..."
 
     if ! whois $domainname >> /dev/null; then
         echo "$domainname seems to be available!"
-        echo "$domainname" >> /potentiallyavailable
+        echo "$domainname" >> /tmp/files/potentiallyavailable
     else
         echo "$domainname seems to be taken! :-("
-        echo "$domainname" >> /listofnames
+        echo "$domainname" >> /tmp/files/tocheck
     fi
 done
